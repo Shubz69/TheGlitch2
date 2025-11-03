c86a925130d2cee5260ab334ca0696bafe085237
Shubz69 <Shubzfx@gmail.com>
2025-11-03 02:44:55 +0000

    Fix nodemailer method name - use createTransport not createTransporter

--- api/auth/forgot-password.js
+++ api/auth/forgot-password.js
@@ -9,7 +9,7 @@ const createEmailTransporter = () => {
   }
 
   try {
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER.trim(),
